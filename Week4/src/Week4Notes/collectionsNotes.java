3cce0b2e8725b370f8fdd8c5856e6cd06be5d56e
yuri-58 <steventorres@gmail.com>
2023-03-29 08:22:42 -0500

    Week 4 push

---
+++ Week4/src/Week4Notes/collectionsNotes.java
@@ -0,0 +1,10 @@
+package Week4Notes;
+
+public class collectionsNotes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Week4/src/Week4Notes/listNotes.java
@@ -0,0 +1,10 @@
+package Week4Notes;
+
+public class listNotes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
